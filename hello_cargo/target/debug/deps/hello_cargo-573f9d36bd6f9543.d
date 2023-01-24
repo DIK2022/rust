6f288f36c8093fc61df2ea422d4ad764b617452a
Денис <106461424+DIK2022@users.noreply.github.com>
2023-01-24 10:17:57 +0300

    Add files via upload

---
+++ hello_cargo/target/debug/deps/hello_cargo-573f9d36bd6f9543.d
@@ -0,0 +1,5 @@
+/home/kdi/Документы/hello_cargo/target/debug/deps/hello_cargo-573f9d36bd6f9543: src/main.rs
+
+/home/kdi/Документы/hello_cargo/target/debug/deps/hello_cargo-573f9d36bd6f9543.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/kdi/Документы/hello_cargo/target/debug/hello_cargo: /home/kdi/Документы/hello_cargo/src/main.rs
